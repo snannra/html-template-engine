c789acee439d0799d4893cc83d36362922d2ad30
snannra <snannra03@gmail.com>
2025-06-16 18:44:55 -0700

    added rust proj

---
+++ temp-eng/target/debug/deps/temp_eng-31d5bf6cba82c2c0.d
@@ -0,0 +1,5 @@
+/home/sohan/Coding/Rust/html-template-engine/temp-eng/target/debug/deps/temp_eng-31d5bf6cba82c2c0: src/main.rs
+
+/home/sohan/Coding/Rust/html-template-engine/temp-eng/target/debug/deps/temp_eng-31d5bf6cba82c2c0.d: src/main.rs
+
+src/main.rs:
---
+++ temp-eng/target/debug/temp-eng.d
@@ -0,0 +1,1 @@
+/home/sohan/Coding/Rust/html-template-engine/temp-eng/target/debug/temp-eng: /home/sohan/Coding/Rust/html-template-engine/temp-eng/src/main.rs
